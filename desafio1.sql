bf070ecee87e33df4f1b85635c656a45affb5855
André De Bem <andrebemferraz@gmail.com>
2021-07-28 15:19:28 -0300

    Request I - Small Update (Order By)

--- desafio1.sql
+++ desafio1.sql
@@ -4,4 +4,5 @@ SELECT COUNTRY_NAME AS País,
               FROM hr.regions AS r
               WHERE r.REGION_NAME = 'Europe'
                 AND c.REGION_ID = r.REGION_ID ), 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM hr.countries AS c;
+FROM hr.countries AS c
+ORDER BY País asc;
