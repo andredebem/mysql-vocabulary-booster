80ca3f927709f154609bf1c04b8b8b4257894b46
André De Bem <andrebemferraz@gmail.com>
2021-07-28 15:17:26 -0300

    Request I - Code Climate Fix I

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 SELECT COUNTRY_NAME AS País,
-       IF(EXISTS
+        IF(EXISTS
             (SELECT *
-             FROM hr.regions AS r
-             WHERE r.REGION_NAME = 'Europe'
-               AND c.REGION_ID = r.REGION_ID ), 'incluído', 'não incluído') AS 'Status Inclusão'
+              FROM hr.regions AS r
+              WHERE r.REGION_NAME = 'Europe'
+                AND c.REGION_ID = r.REGION_ID ), 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS c;
