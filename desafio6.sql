ed129679be888fee4c8cf8c8b0507dedcb0d9ed3
André De Bem <andrebemferraz@gmail.com>
2021-07-28 17:49:38 -0300

    Request VI - Code Climate Fix I

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-       j.JOB_TITLE AS 'Cargo',
-       h.START_DATE AS 'Data de início do cargo',
-       d.DEPARTMENT_NAME AS 'Departamento'
+        j.JOB_TITLE AS 'Cargo',
+        h.START_DATE AS 'Data de início do cargo',
+        d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees e
 INNER JOIN hr.job_history h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 INNER JOIN hr.jobs j ON h.JOB_ID = j.JOB_ID
