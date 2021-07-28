190029ecc35428ae53926ecc676af4dd592ff420
André De Bem <andrebemferraz@gmail.com>
2021-07-28 18:05:19 -0300

    Request VI - Small Fix

--- desafio6.sql
+++ desafio6.sql
@@ -5,5 +5,5 @@ SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 FROM hr.employees e
 INNER JOIN hr.job_history h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 INNER JOIN hr.jobs j ON h.JOB_ID = j.JOB_ID
-INNER JOIN hr.departments d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+INNER JOIN hr.departments d ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC , Cargo ASC;
