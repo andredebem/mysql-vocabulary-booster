acf464bc1ed246d317f0931a9beb1cdd2575ea8b
André De Bem <andrebemferraz@gmail.com>
2021-07-28 15:14:08 -0300

    Request I

---
+++ desafio1.sql
@@ -0,0 +1,7 @@
+SELECT COUNTRY_NAME AS País,
+       IF(EXISTS
+            (SELECT *
+             FROM hr.regions AS r
+             WHERE r.REGION_NAME = 'Europe'
+               AND c.REGION_ID = r.REGION_ID ), 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS c;
