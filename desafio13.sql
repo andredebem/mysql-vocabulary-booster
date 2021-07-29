4b5b91bf9889252303c5ac55b18d82920a5b0626
André De Bem <andrebemferraz@gmail.com>
2021-07-29 11:51:07 -0300

    Request XIII - Code Climate Fix I

--- desafio13.sql
+++ desafio13.sql
@@ -3,7 +3,7 @@ SELECT p.ProductName AS 'Produto',
 FROM w3schools.products p
 WHERE EXISTS
     (SELECT *
-     FROM w3schools.order_details d
-     WHERE p.ProductID = d.ProductID
-       AND d.Quantity > 80)
+      FROM w3schools.order_details d
+      WHERE p.ProductID = d.ProductID
+        AND d.Quantity > 80)
 ORDER BY Produto;
