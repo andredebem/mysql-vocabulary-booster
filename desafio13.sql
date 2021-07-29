d85eea9b72b6430231d03c7c880b71c56d2bebd4
André De Bem <andrebemferraz@gmail.com>
2021-07-29 11:49:34 -0300

    Request XIII

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS 'Produto',
+        p.Price AS 'Preço'
+FROM w3schools.products p
+WHERE EXISTS
+    (SELECT *
+     FROM w3schools.order_details d
+     WHERE p.ProductID = d.ProductID
+       AND d.Quantity > 80)
+ORDER BY Produto;
